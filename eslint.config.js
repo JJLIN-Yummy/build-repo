d2bb0eeb7b736d214992b78444ce42df814c0dde
SanctuaryHome <11550233+sanctuaryhome@user.noreply.gitee.com>
2026-06-14 13:08:47 +0800

    fix(eslint-config): fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,29 @@
-import es from '@build/eslint-config';
+import baseConfig from "@build/eslint-config";
 
-export default es;
+export default [
+  // 全局忽略：先写白名单取反，再黑名单，顺序不能颠倒
+  {
+    ignores: [
+      // 保留 vite-config、utils、babel
+      "!packages/vite-config/**",
+      "!packages/utils/**",
+      "!packages/babel/**",
+      // 其余 *-config 包整体忽略
+      "packages/*-config/**",
+      "node_modules/**",
+    ],
+  },
+
+  // 继承公共eslint规则
+  ...baseConfig,
+
+  // 全局统一给所有packages下深层TS文件注入tsconfig类型信息
+  {
+    files: ["packages/**/**/*.{ts,tsx,mts,cts}"],
+    languageOptions: {
+      parserOptions: {
+        project: ["./packages/*/tsconfig.json"],
+      },
+    },
+  },
+];
