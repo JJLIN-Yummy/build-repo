e9bdf19b1e65772fe4ee34b2260d8fb514a2884d
SanctuaryHome <11550233+sanctuaryhome@user.noreply.gitee.com>
2026-07-10 11:11:51 +0800

    docs(stylelint-config): format

--- packages/stylelint/stylelint.config.js
+++ packages/stylelint/stylelint.config.js
@@ -1,4 +1,4 @@
 export default {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  rules: {}
+  extends: ["stylelint-config-standard", "stylelint-config-prettier"],
+  rules: {},
 };
